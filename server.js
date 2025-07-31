22a5a2afe2e9425c9cc448ef2d47196a0f5cb74e
jackfury99 <tanishpanghal9@gmail.com>
2025-07-31 19:33:04 +0530

    Changed route from /chat to /ask

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.post('/chat', async (req, res) => {
+app.post('/ask', async (req, res) => {
   const message = req.body.message;
 
   try {
